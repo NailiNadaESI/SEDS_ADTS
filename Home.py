87a1734a18de8caea3b37c1af05f4a7f11868725
NailiNadaESI <122780235+NailiNadaESI@users.noreply.github.com>
2024-02-10 18:54:06 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,13 +1,4 @@
 import streamlit as st
 from Pages.Preprocessing import preprocess_app
 
-def main():
-    st.sidebar.title("Navigation")
-    pages = ["Home"]
-    selected_page = st.sidebar.selectbox("Select Page", pages)
-
-    if selected_page == "Home":
-        preprocess_app()
-
-if __name__ == "__main__":
-    main()
+st.write("hiiii")
