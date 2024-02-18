541f12df962f90f9c6bb63d90bc9b29c5594edc7
NailiNada <kn_naili@esi.dz>
2024-02-18 08:17:47 +0100

    project

--- pages/1_🗒️_Preprocessing.py
+++ pages/1_🗒️_Preprocessing.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 import numpy as np
 import pandas as pd
+import os 
 import matplotlib.pyplot as plt
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from statsmodels.tsa.vector_ar.var_model import VAR
@@ -10,74 +11,94 @@ from statsmodels.tsa.vector_ar.var_model import VAR
 st.set_page_config(page_title='Preprocessing ', page_icon='🗒️')
 st.markdown('# 🗒️ Preprocessing :')
 
-def preprocess_app():
-    st.write ("### 1/ Loading the dataset:")
-    train_hours = 100*7*24
-    test_hours = 20*7*24  
+st.write("### 1/ Loading the dataset:")
+train_hours = 80*7*24
+test_hours = 15*7*24  
 
-    df = pd.read_csv('data/bike-and-ped-counter.csv.zip', nrows=train_hours+test_hours, parse_dates=['Date'])
-    st.write(df)
+df = pd.read_csv('data/bike-and-ped-counter.csv.zip', nrows=train_hours+test_hours, parse_dates=['Date'])
+st.write(df)
 
-    st.write("First we need to have a global idea about the dataset by generating  a small description of the dataset")
-    st.table(df.describe())
+st.write("First we need to have a global idea about the dataset by generating a small description of the dataset")
+st.table(df.describe())
 
-    st.write ("### 2/ Handeling Missing values:")
-    st.write("#### Identifying Null values:")
-    st.write('We need to identify the null values in the dataset')
-    st.table(df.isna().sum())
-    st.write("We have found 5 missing values in the dataset")
+st.write("### 2/ Handeling Missing values:")
+st.write("#### Identifying Null values:")
+st.write('We need to identify the null values in the dataset')
+st.table(df.isna().sum())
+st.write("We have found 4 missing values in the dataset")
 
-    st.write("#### Handeling Null values:")
-    df['Ped South'] = df['Ped South'].groupby(df.Date.dt.hour).transform(lambda x: x.fillna(x.mean()))
-    df['Ped North'] = df['Ped North'].groupby(df.Date.dt.hour).transform(lambda x: x.fillna(x.mean()))
-    df['Bike South'] = df['Bike South'].groupby(df.Date.dt.hour).transform(lambda x: x.fillna(x.mean()))
-    df['Bike North'] = df['Bike North'].groupby(df.Date.dt.hour).transform(lambda x: x.fillna(x.mean()))
-    df['BGT North of NE 70th Total'] = df['Ped South'] + df['Ped North'] + df['Bike South'] + df['Bike North']
+st.write("#### Handeling Null values:")
+df['Ped South'] = df['Ped South'].groupby(df.Date.dt.hour).transform(lambda x: x.fillna(x.mean()))
+df['Ped North'] = df['Ped North'].groupby(df.Date.dt.hour).transform(lambda x: x.fillna(x.mean()))
+df['Bike South'] = df['Bike South'].groupby(df.Date.dt.hour).transform(lambda x: x.fillna(x.mean()))
+df['Bike North'] = df['Bike North'].groupby(df.Date.dt.hour).transform(lambda x: x.fillna(x.mean()))
+df['BGT North of NE 70th Total'] = df['Ped South'] + df['Ped North'] + df['Bike South'] + df['Bike North']
 
-    df['Date'] = pd.to_datetime(df['Date'].dt.date)
-    st.write("In order to get rid of the missing values , it's enough to replace them using the mean value :")
-    st.table(df.isna().sum())
-    st.table(df.describe())
+df['Date'] = pd.to_datetime(df['Date'].dt.date)
+st.write("In order to get rid of the missing values, it's enough to replace them using the mean value:")
+st.table(df.isna().sum())
+st.table(df.describe())
 
-    st.write("### 3/ Aggregation :")
-    st.write("Next step is to aggregate the data that we have , so instead of having the data by hour we'll  have them by day while making the day the new index of the dataset")
-    df_day = pd.DataFrame()
-    df_day['Ped South'] = df.groupby(df.Date)['Ped South'].sum()
-    df_day['Ped North'] = df.groupby(df.Date)['Ped North'].sum()
-    df_day['Bike South'] = df.groupby(df.Date)['Bike South'].sum()
-    df_day['Bike North'] = df.groupby(df.Date)['Bike North'].sum()
-    df_day['Total'] = df.groupby(df.Date)['BGT North of NE 70th Total'].sum()
+st.write("### 3/ Aggregation:")
+st.write("Next step is to aggregate the data that we have, so instead of having the data by hour, we'll have them by day while making the day the new index of the dataset")
+df_day = pd.DataFrame()
+df_day['Ped South'] = df.groupby(df.Date)['Ped South'].sum()
+df_day['Ped North'] = df.groupby(df.Date)['Ped North'].sum()
+df_day['Bike South'] = df.groupby(df.Date)['Bike South'].sum()
+df_day['Bike North'] = df.groupby(df.Date)['Bike North'].sum()
+df_day['Total'] = df.groupby(df.Date)['BGT North of NE 70th Total'].sum()
 
-    df_day.index = pd.DatetimeIndex(df_day.index.values, freq=df_day.index.inferred_freq)
-    df_day.to_csv("data/preprocessedData.csv")
-    st.write(df_day)
+df_day.index = pd.DatetimeIndex(df_day.index.values, freq=df_day.index.inferred_freq)
+st.write(df_day)
 
-    st.write("### 4/Plotting The data:")
-    columns = df_day.columns.difference(['Date'])
-    colors = ['blue', 'green', 'red', 'purple', 'pink']
+st.write("### 4/ Plotting The data:")
+columns = df_day.columns.difference(['Date'])
+colors = ['orange', 'green', 'red', 'purple', 'blue']
 
-    fig, axes = plt.subplots(nrows=len(columns), ncols=1, figsize=(16, 6 * len(columns)))
+fig, axes = plt.subplots(nrows=len(columns), ncols=1, figsize=(16, 6 * len(columns)))
 
-    for clm, color, ax in zip(columns, colors, axes):
-      df_day[clm].plot(ax=ax, color=color)
-      ax.set_xlabel('Date')
-      ax.set_ylabel('Values')
-      ax.set_title(f'Plot of {clm}')
-    
-    plt.tight_layout()
-    st.pyplot(fig)
+for clm, color, ax in zip(columns, colors, axes):
+  df_day[clm].plot(ax=ax, color=color)
+  ax.set_xlabel('Date')
+  ax.set_ylabel('Values')
+  ax.set_title(f'Plot of {clm}')
 
-    fig, ax = plt.subplots(figsize=(16, 6))
-    for clm, color in zip(columns, colors):
-       df_day[clm].plot(ax=ax, color=color, label=clm)
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Values')
-    ax.set_title('Plot of All Columns')
-    ax.legend()
-    st.pyplot(fig)
+plt.tight_layout()
+st.pyplot(fig)
 
-    df_day[:(train_hours//24)].to_csv('data/PreprocessedData_Train.csv')
-    df_day[:(test_hours//24)].to_csv('data/PreprocessedData_Test.csv')
-    
+fig, ax = plt.subplots(figsize=(16, 6))
+for clm, color in zip(columns, colors):
+   df_day[clm].plot(ax=ax, color=color, label=clm)
+ax.set_xlabel('Date')
+ax.set_ylabel('Values')
+ax.set_title('Plot of All Columns')
+ax.legend()
+st.pyplot(fig)
 
-preprocess_app()
+train = df_day[:(train_hours//24)].copy()
+train.to_csv('data/train.csv')
+st.write(" ###### Plotting the autocorrelation:")
+
+fig, ax = plt.subplots(figsize=(16, 6))
+pd.plotting.autocorrelation_plot(df_day.iloc[:train_hours]['Total'], ax=ax)
+ax.set_title("Autocorrelation Plot")
+st.pyplot(fig)
+
+st.write("We need to remove the long term seasonality")
+
+month_mean_train = train.groupby(train.index.month).mean()
+train['Ped South'] = train.apply(lambda x: x['Ped South'] - month_mean_train['Ped South'][x.name.month], axis=1)
+train['Ped North'] = train.apply(lambda x: x['Ped North'] - month_mean_train['Ped North'][x.name.month], axis=1)
+train['Bike South'] = train.apply(lambda x: x['Bike South'] - month_mean_train['Bike South'][x.name.month], axis=1)
+train['Bike North'] = train.apply(lambda x: x['Bike North'] - month_mean_train['Bike North'][x.name.month], axis=1)
+train['Total'] = train.apply(lambda x: x['Total'] - month_mean_train['Total'][x.name.month], axis=1)
+
+fig, ax = plt.subplots(figsize=(16, 6))
+pd.plotting.autocorrelation_plot(train['Total'],ax=ax)
+ax.set_title("Autocorrelation Plot")
+st.pyplot(fig)
+
+if not os.path.exists('data/PreprocessedData_Train.csv'):
+  train.to_csv('data/PreprocessedData_Train.csv')
+if not os.path.exists('data/PreprocessedData_Test.csv'):
+   df_day[(test_hours//24):].to_csv('data/PreprocessedData_Test.csv')
--- pages/2_💻_Model.py
+++ pages/2_💻_Model.py
@@ -3,16 +3,100 @@ import pandas as pd
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 from statsmodels.tsa.statespace.sarimax import SARIMAX
-from statsmodels.tsa.vector_ar.var_model import VAR
 
 import streamlit as st
 
 st.set_page_config(page_title='Modeling ', page_icon='💻')
 st.markdown('# 💻 Modeling :')
 
-st.write ("### 1/ The dataset:")
+st.write ("### The training dataset:")
 train = pd.read_csv('data/PreprocessedData_Train.csv')
 train.rename(columns={"Unnamed: 0":"Date"},inplace=True)
+train['Date'] = pd.to_datetime(train['Date'])
 train.set_index("Date", inplace=True)
-
 st.write(train)
+
+st.write ("## 1/ Univariante Anomaly detection :")
+st.write ("In this project I chose to go with studing the anomalies based on one variable that is the total using the ARIMA algorithm")
+st.write (" First we'll try find the best model based on the best AIC and the best Order")
+st.write("- **Order(p,d,q)** : it's a set of parameters defining the structure of a model  : ")
+st.write ("p (Autoregression Order) : represents the number of past time points used to predict the current value , the higher the better ." )
+st.write ("d (Differencing Order) : represents the numberof times the raw observations are differenced to make the time series stationary, it only takes 0 or 1")
+st.write ("q (Moving average Order) : indecates the number of past forecast errors used to predict the current value")
+
+st.write("- **AIC(Akaike Information Criterion)** : The AIC is a measure of the model's goodness of fit, taking into account the complexity of the model. the lower the AIC the better")
+
+def Arima():
+    AIC = {}
+    best_aic, best_order = np.inf, 0
+
+    for p in range(6, 9):
+        for q in range(0, 10):
+            mod = SARIMAX(train['Total'], order=(p, 0, q), enforce_invertibility=False)
+            try:
+                res = mod.fit(disp=False)
+                AIC[(p, 0, q)] = res.aic
+            except:
+                AIC[(p, 0, q)] = np.inf
+            if AIC[(p, 0, q)] < best_aic:
+                best_aic = AIC[(p, 0, q)]
+                best_order = (p, 0, q)
+
+    with open('Arima.txt', 'w') as file:
+        file.write(str(best_order) + '\n')
+        file.write(str(best_aic))
+    return best_order, best_aic
+def read_Arima_results():
+    try:
+        with open('Arima.txt', 'r') as file:
+            lines = file.readlines()
+            best_order = tuple(map(int, lines[0].strip()[1:-1].split(',')))
+            best_aic = float(lines[1].strip())
+
+            return best_order, best_aic
+
+    except FileNotFoundError:
+        Arima()
+
+best_order, best_aic = read_Arima_results()
+
+st.write("After applying the algorithm one the dataset we found that : ")
+st.write('BEST ORDER : '+str(best_order))
+st.write('BEST AIC:', str(best_aic))
+
+st.write("Now we have to train the best univariante model and visualize it's performance ")
+mod = SARIMAX(train['Total'], order=best_order, enforce_invertibility=False)
+res = mod.fit(disp=False)
+
+st.pyplot(res.plot_diagnostics(figsize=(18,10)))
+ 
+
+st.write("#### train and plot fitted values : ")
+trained = pd.read_csv('data/train.csv')
+
+trained.rename(columns={"Unnamed: 0": "Date"}, inplace=True)
+trained['Date'] = pd.to_datetime(trained['Date'])
+
+trained.set_index("Date", inplace=True)
+month_mean_train = trained.groupby(trained.index.month).mean()
+predict = res.get_prediction()
+predicted_mean = predict.predicted_mean + predict.predicted_mean.index.month.map(month_mean_train['Total'])
+train_uni = train['Total'] + train.index.month.map(month_mean_train['Total'])
+predict_ci = predict.conf_int(alpha=0.1)
+predict_ci['lower Total'] = predict_ci.apply(lambda x: x['lower Total'] + month_mean_train['Total'][x.name.month], axis=1)
+predict_ci['upper Total'] = predict_ci.apply(lambda x: x['upper Total'] + month_mean_train['Total'][x.name.month], axis=1)
+
+residuals_mean = res.resid.mean()
+residuals_std = res.resid.std()
+
+fig, ax = plt.subplots(figsize=(15, 6))
+
+ax.plot(train_uni, 'k.', label='Actual Data')
+ax.plot(predicted_mean, linestyle='--', linewidth=2, color='blue', label='Predicted Mean')
+ax.fill_between(predict_ci.index, predict_ci['lower Total'], predict_ci['upper Total'], alpha=0.6, label='Confidence Interval')
+
+ax.set_xlabel('Date')
+ax.legend()
+
+# Display the plot in Streamlit
+st.pyplot(fig)
